8fcd4f63362405a561639aa0b591478f46b8bd6b
wyu <kharma@gmail.com>
2020-06-11 23:17:02 -0700

    add steps/citations

--- run_analysis.R
+++ run_analysis.R
@@ -41,6 +41,7 @@ print(c(features[,2])) # verify output
 
 # step 1C: label columns before merging datasets
 # Note: Measurement columns left to be labeled by index only
+# source: https://stackoverflow.com/questions/7531868/how-to-rename-a-single-column-in-a-data-frame
 colnames(train_subject) <- c("subject_id")
 head(train_subject) # verify colnames
 colnames(train_y) <- c("label_id")
@@ -51,8 +52,9 @@ head(test_subject) # verify colnames
 colnames(test_y) <- c("label_id")
 head(test_y) # verify colnames
 
-# step 1D: combine train/test data into single dataset 
+# step 1D: combine train/test data into single dataset with cbind() 
 # Note: merge data by column; cbind data by subject, labels/y and data/x
+# source: https://stat.ethz.ch/R-manual/R-devel/library/base/html/cbind.html
 df_train <- data.frame(cbind(train_subject, train_y, train_x))
 dim(df_train) # verify that column count will line up between datasets
 head(df_train)
@@ -63,15 +65,17 @@ head(df_test)
 
 # Note: Each record is organized by subject, activity and measurement
 # rbind train/test data together per instructions
+# source: https://stat.ethz.ch/R-manual/R-devel/library/base/html/cbind.html
 df_total <- data.frame(rbind(df_train, df_test))
 dim(df_total) # verify dim and colnames after rbind
 colnames(df_total)
 
-# step 2A: extract only mean and standard deviation measurements 
+# step 2A: filter for only mean and standard deviation measurements 
 # https://stackoverflow.com/questions/4220631/how-do-i-grep-in-r
 df_features <- data.frame(features) # read in all measurement names
 
 # filter measurement names by "mean", "Mean" and "std"
+# source: https://stackoverflow.com/questions/7597559/grep-using-a-character-vector-with-multiple-patterns
 match_mean <- df_features$id[grep("mean", df_features$value)]
 match_mean_cap <- df_features$id[grep("Mean", df_features$value)]
 match_std <- df_features$id[grep("std", df_features$value)]
@@ -82,12 +86,13 @@ match_mean_cap
 match_std
 
 # step 2B: subset by column index based on step 2A results
+# source: https://stackoverflow.com/questions/45846341/how-to-subset-a-data-frame-column-wise-using-column-names
 df_mean_std <- subset(df_total, select=c("subject_id","label_id","V1","V2","V3","V41","V42","V43","V81","V82","V83","V121","V122","V123","V161","V162","V163","V201","V214","V227","V240","V253","V266","V267","V268","V294","V295","V296","V345","V346","V347","V373","V374","V375","V424","V425","V426","V452","V453","V454","V503","V513","V516","V526","V529","V539","V542","V552","V4","V5","V6","V44","V45","V46","V84","V85","V86","V124","V125","V126","V164","V165","V166","V202","V215","V228","V241","V254","V269","V270","V271","V348","V349","V350","V427","V428","V429","V504","V517","V530","V543","V555","V556","V557","V558","V559","V560","V561"))
 dim(df_mean_std) # verify dim, head and colnames are aligned within dataset
 head(df_mean_std)
 colnames(df_mean_std)
 
-# step 3A: replace activity labels per instructions as follows:
+# step 3: replace activity labels per instructions as follows:
 # 1 WALKING
 # 2 WALKING_UPSTAIRS
 # 3 WALKING_DOWNSTAIRS
@@ -104,7 +109,7 @@ df_mean_std$label_id[df_mean_std$label_id == 5] <- "STANDING"
 df_mean_std$label_id[df_mean_std$label_id == 6] <- "LAYING"
 df_mean_std$label_id
 
-# step 4A: replace activity labels
+# step 4: replace activity labels
 # Note: measurement column names left as index and mapped in code book
 
 # step 5A: aggregate measurements by subject and activity
